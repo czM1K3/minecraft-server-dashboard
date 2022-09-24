524a0cc9e052edc75288869409a53d766763b3ae
czM1K3 <misamadera1@gmail.com>
2022-09-25 01:20:27 +0200

    Error page

--- components/Error.tsx
+++ components/Error.tsx
@@ -4,8 +4,13 @@ import Layout from "./Layout.tsx";
 const Error: FunctionalComponent = () => {
 	return (
 		<Layout>
-			<div>
-				<h1>Something went wrong</h1>
+			<div
+				className="justify-content-center align-items-center d-flex vh-100 flex-column"
+				style={{
+					height: "max-content"
+				}}
+			>
+				<h1 className="text-white">Something went wrong</h1>
 			</div>
 		</Layout>
 	)
--- controllers/minecraftApi.ts
+++ controllers/minecraftApi.ts
@@ -4,21 +4,29 @@ const serverAddress = Deno.env.get("SERVER_ADDRESS");
 if (!serverAddress) throw new Error("SERVER_ADDRESS is not defined");
 
 const ServerRoute = async () => {
-	const result = await fetch(`${serverAddress}/v1/server`);
-	if (result.ok) {
-		const json = await result.json();
-		return json as ServerRouteType;
+	try {
+		const result = await fetch(`${serverAddress}/v1/server`);
+		if (result.ok) {
+			const json = await result.json();
+			return json as ServerRouteType;
+		}
+		return null;
+	} catch {
+		return null;
 	}
-	return null;
 };
 
 const PlayersRoute = async () => {
-	const result = await fetch(`${serverAddress}/v1/players`);
-	if (result.ok) {
-		const json = await result.json();
-		return json as PlayersRouteType;
+	try {
+		const result = await fetch(`${serverAddress}/v1/players`);
+		if (result.ok) {
+			const json = await result.json();
+			return json as PlayersRouteType;
+		}
+		return null;
+	} catch {
+		return null;
 	}
-	return null;
 };
 
 const MinecraftApiController = {
